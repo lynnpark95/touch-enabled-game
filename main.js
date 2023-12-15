e1e60a0458f97a704168e9873780e1cc92d310f7
lynnpark95 <lynn.hpark@outlook.com>
2023-12-14 23:48:09 -0700

    Function added

--- main.js
+++ main.js
@@ -1,32 +1,91 @@
-const diamond = document.getElementById("diamond");
 const circle = document.getElementById("circle");
-let dragging = false;
+const diamond = document.getElementById("diamond");
+const rectangle = document.getElementById("rectangle");
+const text = document.getElementById("text");
+const plane = document.getElementById("plane");
 
-circle.addEventListener("touchstart", (e) => {
+let dragging = false;
+let crossedDiamond = false;
+let enteredRectangle = false;
+
+circle.addEventListener("touchstart", dragStart);
+circle.addEventListener("touchend", dragEnd);
+circle.addEventListener("touchmove", drag);
+
+function dragStart(e) {
   dragging = true;
   circle.style.zIndex = "3";
-});
+}
 
-document.addEventListener("touchmove", (e) => {
-  if (!dragging) return;
-
-  const rect = diamond.getBoundingClientRect();
-  const touchX = e.touches[0].clientX;
-  const touchY = e.touches[0].clientY;
-
-  if (
-    touchX >= rect.left &&
-    touchX <= rect.right &&
-    touchY >= rect.top &&
-    touchY <= rect.bottom
-  ) {
-    circle.classList.add("circleOnDia");
-  } else {
-    circle.classList.remove("circleOnDia");
-  }
-});
-
-document.addEventListener("touchend", () => {
+function dragEnd() {
   dragging = false;
   circle.style.zIndex = "2";
-});
+
+  if (crossedDiamond && enteredRectangle) {
+    circle.style.backgroundColor = "yellow";
+    setTimeout(() => {
+      hideShapes();
+      plane.classList.add("show");
+      text.classList.add("show");
+      text.style.display = "block";
+
+      const planeRect = plane.getBoundingClientRect();
+      text.style.left = `${planeRect.right}px`;
+      text.style.top = `${planeRect.top}px`;
+
+      moveTextWithPlane();
+    }, 500);
+  } else {
+    if (!crossedDiamond && enteredRectangle) {
+      circle.style.left = "40vw";
+      circle.style.top = "80vw";
+    }
+  }
+}
+
+function moveTextWithPlane() {
+  const interval = setInterval(() => {
+    const planeRect = plane.getBoundingClientRect();
+    if (planeRect.right > 0 && plane.classList.contains("show")) {
+      const textHeight = text.offsetHeight;
+      const centerY = planeRect.top + planeRect.height / 2 - textHeight / 2;
+
+      text.style.left = `${planeRect.right}px`;
+      text.style.top = `${centerY}px`;
+    } else {
+      clearInterval(interval);
+    }
+  }, 10);
+}
+
+function drag(e) {
+  if (dragging) {
+    e.preventDefault();
+    const touchX = e.touches[0].clientX - 25;
+    const touchY = e.touches[0].clientY - 25;
+    circle.style.left = `${touchX}px`;
+    circle.style.top = `${touchY}px`;
+
+    if (!crossedDiamond && isOverlapping(circle, diamond)) {
+      crossedDiamond = true;
+    }
+    enteredRectangle = isOverlapping(circle, rectangle);
+  }
+}
+
+function isOverlapping(element1, element2) {
+  const rect1 = element1.getBoundingClientRect();
+  const rect2 = element2.getBoundingClientRect();
+  return !(
+    rect1.right < rect2.left ||
+    rect1.left > rect2.right ||
+    rect1.bottom < rect2.top ||
+    rect1.top > rect2.bottom
+  );
+}
+
+function hideShapes() {
+  document.querySelectorAll(".shape").forEach((shape) => {
+    shape.style.display = "none";
+  });
+}
